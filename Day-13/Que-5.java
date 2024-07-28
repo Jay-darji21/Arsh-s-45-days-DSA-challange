cb4ee2e35220d175ee5bcd23aab45b76234c9a41
Jay Darji <darjijay214@gmail.com>
2024-07-28 20:15:24 +0530

    first message

---
+++ Day-13/Que-5.java
@@ -0,0 +1,34 @@
+// 496. Next Greater Element I
+// Easy
+
+// TimeComplexity : O(n+m)
+// SpaceComplexity : O(n)
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        Stack<Integer> stack = new Stack<>();
+        Map<Integer,Integer> map = new HashMap<>();
+
+        for(int i=nums2.length -1 ; i>=0 ; i--){
+            while(!stack.isEmpty() && stack.peek() <= nums2[i]){
+                stack.pop();
+            }
+            if(stack.isEmpty()){
+                map.put(nums2[i],-1);
+            }else{
+                map.put(nums2[i],stack.peek());
+            }
+            stack.push(nums2[i]);
+        }   
+
+        for(int i=0 ; i<nums1.length ; i++){
+            nums1[i] = map.get(nums1[i]);
+        }
+
+        return nums1;
+    }
+}
