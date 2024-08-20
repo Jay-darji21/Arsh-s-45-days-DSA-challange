6704b7e7322d06db0c106b0f593b46c0137634bd
Jay Darji <darjijay214@gmail.com>
2024-08-20 17:22:42 +0530

    first commit

---
+++ Day-35/Que-3.java
@@ -0,0 +1,29 @@
+// 264. Ugly number II
+// Meidum
+
+import java.util.HashSet;
+import java.util.PriorityQueue;
+
+class Solution {
+    public int nthUglyNumber(int n) {
+        int[] primes = {2,3,5};
+        PriorityQueue<Long> uglyHeap = new PriorityQueue<>();
+        HashSet<Long> visited = new HashSet<>();
+
+        uglyHeap.add(1L);
+        visited.add(1L);
+
+        long curr = 1L;
+        for(int i=0; i<n ; i++){
+            curr = uglyHeap.poll();
+            for(int prime : primes){
+                long new_ugly = curr*prime;
+                if(!visited.contains(new_ugly)){
+                    uglyHeap.add(new_ugly);
+                    visited.add(new_ugly);
+                }
+            }
+        }
+        return (int)curr;
+    }
+}
