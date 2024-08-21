97f1e4a15ee341cea06d137570000971c4278bb1
Jay Darji <darjijay214@gmail.com>
2024-08-21 21:28:42 +0530

    first commit

---
+++ Day-37/Que-2.java
@@ -0,0 +1,34 @@
+//901. Online stock span
+// Medium
+
+import java.util.ArrayList;
+import java.util.Stack;
+
+class StockSpanner {
+    Stack<Integer> st;
+    ArrayList<Integer> list;
+
+    public StockSpanner() {
+        st = new Stack<>();
+        list = new ArrayList<>();
+    }
+    
+    public int next(int price) {
+        list.add(price);
+        while(!st.isEmpty() && list.get(st.peek())<=price){
+            st.pop();
+        }
+
+        int pre = st.isEmpty()?-1:st.peek();
+        int ans = (list.size()-1)-pre;
+        st.push(list.size()-1);
+
+        return ans;
+    }
+}
+
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * StockSpanner obj = new StockSpanner();
+ * int param_1 = obj.next(price);
+ */
